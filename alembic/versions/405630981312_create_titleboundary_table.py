72308230bb9f04cae837c52a4c864a2f7c2e78d0
Einari Naukkarinen <einari.naukkarinen@outlook.com>
2025-01-27 10:28:53 +0000

    feat: alembic and docker

---
+++ alembic/versions/405630981312_create_titleboundary_table.py
@@ -0,0 +1,55 @@
+"""create titleboundary table
+
+Revision ID: 405630981312
+Revises: 
+Create Date: 2025-01-24 11:45:00.214257
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+import geoalchemy2
+
+
+# revision identifiers, used by Alembic.
+revision: str = "405630981312"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "title_boundary",
+        sa.Column("id", sa.String(255), primary_key=True),  # Primary key column
+        sa.Column("dataset", sa.String(255), nullable=False),
+        sa.Column("end_date", sa.String(255), nullable=True),
+        sa.Column("entity", sa.String(255), nullable=False),
+        sa.Column("entry_date", sa.String(255), nullable=False),
+         sa.Column(
+            "geometry",
+            geoalchemy2.types.Geometry(
+                srid=4326,
+                spatial_index=False,
+                from_text="ST_GeomFromEWKT",
+                name="geometry",
+                nullable=False,
+            ),
+            nullable=False,
+        ),
+        sa.Column("name", sa.String(255), nullable=True),
+        sa.Column("organisation_entity", sa.String(255), nullable=False),
+        sa.Column("point", sa.String, nullable=False),
+        sa.Column("prefix", sa.String(255), nullable=False),
+        sa.Column("reference", sa.String(255), nullable=False),
+        sa.Column("start_date", sa.String(255), nullable=False),
+        sa.Column("typology", sa.String(255), nullable=False),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("title_boundary")  # nosec - B608: SQLAlchemy injection is not a concern in this context
+    pass
